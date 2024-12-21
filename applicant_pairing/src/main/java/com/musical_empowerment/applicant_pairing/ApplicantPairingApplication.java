53f0222b6108a390ea3cb3e1f5c6a207f3baea1e
ntjohns1 <nelsontjohns@gmail.com>
2024-12-20 23:20:19 -0500

    init

---
+++ applicant_pairing/src/main/java/com/musical_empowerment/applicant_pairing/ApplicantPairingApplication.java
@@ -0,0 +1,13 @@
+package com.musical_empowerment.applicant_pairing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantPairingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicantPairingApplication.class, args);
+	}
+
+}
---
+++ applicant_pairing/src/test/java/com/musical_empowerment/applicant_pairing/ApplicantPairingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musical_empowerment.applicant_pairing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicantPairingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
